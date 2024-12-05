cbb596b2bf1476e2b28fe9ca1350088eeea9237b
Justin Hendrix <jhendrix13@gmail.com>
2024-12-04 22:15:04 -0800

    sending test notification/msg to app

--- main.py
+++ main.py
@@ -13,46 +13,55 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from .db import engine, SessionLocal
-import firebase_admin
-from firebase_admin import credentials
+from .fb import test_send
 
 # models.Base.metadata.drop_all(bind=engine)
 models.Base.metadata.create_all(engine)
+
 
 def get_db():
     db = SessionLocal()
     try:
         yield db
-    finally: db.close()
+    finally:
+        db.close()
+
 
 SessionDep = Annotated[Session, Depends(get_db)]
 
 app = FastAPI()
 
+
 @app.get('/')
 def read_root():
     return {}
 
+
 @app.get('/msgtest')
 def msg_test():
-    msg = firebase_admin.messaging.Message()
-    firebase_admin.messaging.send(msg)
+    print('attempting test send')
+    test_send()
     return {}
-    
+
+
 @app.get('/users/{user_id}', response_model=schemas.User)
 def read_user(user_id: int, db: SessionDep):
-    user = db.scalars(select(models.User).where(models.User.id == user_id)).first()
+    user = db.scalars(select(models.User).where(
+        models.User.id == user_id)).first()
     return user
+
 
 @app.get('/users', response_model=schemas.User)
 def read_user(uid: str, db: SessionDep):
-    user = db.scalars(select(models.User).where(models.User.firebase_uid == uid)).first()
+    user = db.scalars(select(models.User).where(
+        models.User.firebase_uid == uid)).first()
 
-    if(user):
+    if (user):
         return user
     else:
-        raise HTTPException(status_code=404, detail='Associated user cannot be found.')
-    
+        raise HTTPException(
+            status_code=404, detail='Associated user cannot be found.')
+
 
 @app.post('/users/create', response_model=schemas.User)
 def create_user(request: schemas.UserCreate, db: SessionDep):
@@ -63,9 +72,11 @@ def create_user(request: schemas.UserCreate, db: SessionDep):
         db.refresh(user)
     except IntegrityError as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail='Firebase UID already exists.')
-    
+        raise HTTPException(
+            status_code=400, detail='Firebase UID already exists.')
+
     return user
+
 
 @app.websocket('/ws')
 async def websocket_endpoint(websocket: WebSocket):
